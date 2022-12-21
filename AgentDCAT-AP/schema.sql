462a7c4d10595c9ffb295113a34432bee00ffa0a
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:11:50 +0100

    test of SQL export

---
+++ AgentDCAT-AP/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model AgentDCAT-AP of the subject dataModel.DCAT-AP for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE AgentDCAT-AP_type AS ENUM ('AgentDCAT-AP');
+CREATE TABLE AgentDCAT-AP (address json, agentName json, agentType text, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type AgentDCAT-AP_type);
