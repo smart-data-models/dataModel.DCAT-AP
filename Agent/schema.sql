8dc5b75218ca4df5797924c95127561260d49b95
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:05:38 +0200

    test of SQL export

--- Agent/schema.sql
+++ Agent/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Agent of the subject dataModel.DCAT-AP for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Agent_type AS ENUM ('Agent');
-CREATE TABLE Agent (Type TEXT, address JSON, areaServed TEXT, name JSON, type Agent_type);
+CREATE TABLE Agent (Type TEXT, address JSON, areaServed TEXT, id TEXT PRIMARY KEY, location JSON, name JSON, type Agent_type);
