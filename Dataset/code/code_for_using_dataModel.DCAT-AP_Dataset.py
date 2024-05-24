5abfb7b9b58abcb12dfd7c994382e1831ea1ff42
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:30:50 +0200

    created example of code

--- Dataset/code/code_for_using_dataModel.DCAT-AP_Dataset.py
+++ Dataset/code/code_for_using_dataModel.DCAT-AP_Dataset.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Dataset"
 subject = "dataModel.DCAT-AP"
-modified = "{'type': 'Property', 'value': {'@type': 'DateTime', '@value': '2021-07-01T10:27:59Z'}}"
+modified = "2015-07-13T03:09:32Z"
 attribute = "modified"
 value = modified
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-title = {'type': 'Property', 'value': ['Alojamientos turísticos en sus diferentes modalidades y categorias de la Comunidad de Madrid']}
+title = ['Alojamientos turísticos en sus diferentes modalidades y categorias de la Comunidad de Madrid']
 attribute = "title"
 value = title
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-contactPoint = {'type': 'Property', 'value': ['https://datos.gob.es/es/comment/reply/145778.']}
+contactPoint = ['https://datos.gob.es/es/comment/reply/145778.']
 attribute = "contactPoint"
 value = contactPoint
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-belongsToCatalogue = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:Catalogue:items:MWVK:61846917'}"
+belongsToCatalogue = "urn:ngsi-ld:Catalogue:items:MWVK:61846917"
 attribute = "belongsToCatalogue"
 value = belongsToCatalogue
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
