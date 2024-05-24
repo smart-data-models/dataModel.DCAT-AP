30f666a6ba896fa7b2dacd76d46f9dbd7b93de1b
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:30:43 +0200

    created example of code

--- DataService/code/code_for_using_dataModel.DCAT-AP_DataService.py
+++ DataService/code/code_for_using_dataModel.DCAT-AP_DataService.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "DataService"
 subject = "dataModel.DCAT-AP"
-accessRights = "{'type': 'Property', 'value': 'No restrictions to access the data but APi requests limit, 5000 requests per hour'}"
+accessRights = "No restrictions to access the data but APi requests limit, 5000 requests per hour"
 attribute = "accessRights"
 value = accessRights
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-endPointDescription = {'type': 'Property', 'value': ['SPARQL end point without authentication', 'API compliant with CKAN specification']}
+endPointDescription = ['SPARQL end point without authentication', 'API compliant with CKAN specification']
 attribute = "endPointDescription"
 value = endPointDescription
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-endPointURL = {'type': 'Property', 'value': ['urn:ngsi-ld:DataServiceDCAT-AP:items:AFGI:79071729', 'urn:ngsi-ld:DataServiceDCAT-AP:items:JAZP:97999812']}
+endPointURL = ['urn:ngsi-ld:DataServiceDCAT-AP:items:AFGI:79071729', 'urn:ngsi-ld:DataServiceDCAT-AP:items:JAZP:97999812']
 attribute = "endPointURL"
 value = endPointURL
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-license = "{'type': 'Property', 'value': 'EUPL.'}"
+license = "EUPL."
 attribute = "license"
 value = license
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
