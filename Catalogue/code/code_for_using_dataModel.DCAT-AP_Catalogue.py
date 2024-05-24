0fde6e2ce32833d5cecd0f51532aefbdd77d0790
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:30:36 +0200

    created example of code

--- Catalogue/code/code_for_using_dataModel.DCAT-AP_Catalogue.py
+++ Catalogue/code/code_for_using_dataModel.DCAT-AP_Catalogue.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Catalogue"
 subject = "dataModel.DCAT-AP"
-catalog = {'type': 'Relationship', 'value': ['urn:ngsi-ld:Catalogue:items:LZMQ:44249979', 'urn:ngsi-ld:Catalogue:items:PECX:02526105']}
+catalog = ['urn:ngsi-ld:Catalogue:items:LZMQ:44249979', 'urn:ngsi-ld:Catalogue:items:PECX:02526105']
 attribute = "catalog"
 value = catalog
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-creator = {'type': 'Relationship', 'object': ['']}
+creator = ['Role fact sport shoulder blue direction probably order.']
 attribute = "creator"
 value = creator
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-dataset = {'type': 'Relationship', 'object': ['urn:ngsi-ld:Catalogue:dataset:ZBCW:95668818']}
+dataset = ['urn:ngsi-ld:Catalogue:dataset:ZBCW:95668818']
 attribute = "dataset"
 value = dataset
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-hasPart = {'type': 'Relationship', 'object': ['urn:ngsi-ld:Catalogue:hasPart:GVZM:66676591']}
+hasPart = ['urn:ngsi-ld:Catalogue:hasPart:GVZM:66676591']
 attribute = "hasPart"
 value = hasPart
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
